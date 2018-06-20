82f62ce2471b9f9a429cc2f69ba6f1e732250961
Yomar Miranda <mirandayomar@gmail.com>
2018-06-19 19:43:45 -0500

    Fix - Añadiendo los datos filtrados a la UI.

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,10 +1,15 @@
 function data(state, action){
 	switch(action.type){
 		case 'SEARCH_VIDEO': {
-			const list = state.data.categories[2].playlist;
-			const results = list.filter((item) => {
-				return item.author.includes(action.payload.query)
-			})
+			let results = [];
+
+			if(action.payload.query){
+				const list = state.data.categories[2].playlist;
+				results = list.filter((item) => {
+					return item.author.includes(action.payload.query)
+				})	
+			}
+		
 			return {
 				...state,
 				search: results 
