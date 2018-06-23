7cf848be2f32da2c6f90b9ceba5e73b476dcc782
Yomar Miranda <mirandayomar@gmail.com>
2018-06-22 21:25:37 -0500

    Action Types.

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -1,5 +1,6 @@
 import data from '../schemas/index';
 import { fromJS } from 'immutable';
+import { OPEN_MODAL, CLOSE_MODAL } from '../action-types/index';
 
 const initialState = fromJS({
 	visibility: false,
@@ -8,12 +9,12 @@ const initialState = fromJS({
 
 function modal(state = initialState, action){
 	switch(action.type){
-		case 'OPEN_MODAL':
+		case OPEN_MODAL:
 			return state.merge({
 				visibility: true,
 				mediaId: action.payload.mediaId
 			})
-		case 'CLOSE_MODAL':
+		case CLOSE_MODAL:
 			return state.set('visibility', false)
 		default:
 			return state
